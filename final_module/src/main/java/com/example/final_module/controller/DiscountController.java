6e90b038eb6634a2cf89476790bc5e666977925c
KimHuong1901 <trankimhuong1901@gmail.com>
2025-01-15 11:17:19 +0700

    final_module4

--- final_module/src/main/java/com/example/final_module/controller/DiscountController.java
+++ final_module/src/main/java/com/example/final_module/controller/DiscountController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -40,13 +41,13 @@ public class DiscountController {
         return"addNew";
     }
     @PostMapping("/add")
-    public String addNewDiscount(@ModelAttribute("discount") Discount discount,
+    public String addNewDiscount(@Validated @ModelAttribute("discount") Discount discount,
                                  BindingResult bindingResult,
                                  RedirectAttributes redirectAttributes,
                                  Model model){
         if(bindingResult.hasErrors()){
             model.addAttribute("error", bindingResult.getAllErrors());
-            return"discounts";
+            return"addNew";
         }
         if(discount.getId()==null || discountSevice.getDiscountById(discount.getId())==null){
             discountSevice.addNewDiscount(discount);
@@ -66,7 +67,7 @@ public class DiscountController {
         }
     }
     @PostMapping("/{id}/update")
-    public String updateDiscount(@PathVariable("id") int id, @ModelAttribute("discount") Discount discount,
+    public String updateDiscount( @Validated @PathVariable("id") int id, @ModelAttribute("discount") Discount discount,
                                  BindingResult bindingResult,
                                  RedirectAttributes redirectAttributes,
                                  Model model){
